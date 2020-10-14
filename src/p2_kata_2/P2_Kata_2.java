bc2031bbd8452bc42d60ac3d1cf24ebb31c2c9d7
kikenky <aaronhdezalv@hotmail.com>
2020-10-14 13:29:38 +0100

    Update 14/10/2020 P2_Kata2_v4_coregido_4

--- src/p2_kata_2/P2_Kata_2.java
+++ src/p2_kata_2/P2_Kata_2.java
@@ -16,10 +16,11 @@ public class P2_Kata_2 {
 
     public static void main(String[] args) {
         Integer[] data = {3,2,3,4,5,3,1,4,5,2,2,1,4,5,3};
-        Histogram histogram = new Histogram(data);
-        Iterator <Map.Entry<Integer,Integer>> entries = histogram.getHistogram().entrySet().iterator();
+        String[] data2 = {"pepe", "paco", "pepe", "maria", "pepe", "paco", "pepe", "maria"};
+        Histogram histogram = new Histogram(data2);
+        Iterator <Map.Entry<String,Integer>> entries = histogram.getHistogram().entrySet().iterator();
         while (entries.hasNext()) {
-            Map.Entry<Integer,Integer> entry = entries.next();
+            Map.Entry<String,Integer> entry = entries.next();
             System.out.println("Key: " + entry.getKey() + ", value: " + entry.getValue());
         }   
     }
